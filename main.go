a877d70d1150f06472052e00d016e4512a1f0602
Andy Huang <19huanga@gmail.com>
2023-05-04 16:43:44 -0400

    fixed

--- main.go
+++ main.go
@@ -5,21 +5,23 @@ import (
 	"strings"
 )
 
-type Machine struct {
+type enigma struct {
 	ALPHABET  string
 	rotors    []*Rotor
 	reflector *Reflector
+	plugboard *Plugboard
 }
 
-func NewMachine(rotors []*Rotor, reflector *Reflector) *Machine {
-	return &Machine{
+func NewEnigma(rotors []*Rotor, reflector *Reflector, plugboard *Plugboard) *enigma {
+	return &enigma{
 		ALPHABET:  "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+		plugboard: plugboard,
 		rotors:    rotors,
 		reflector: reflector,
 	}
 }
 
-func (m *Machine) Encipher(text string) string {
+func (m *enigma) Encipher(text string) string {
 	var result strings.Builder
 	for _, x := range strings.ToUpper(text) {
 		result.WriteString(string(m.encipherCharacter(x)))
@@ -27,50 +29,59 @@ func (m *Machine) Encipher(text string) string {
 	return result.String()
 }
 
-func (m *Machine) Decipher(text string) string {
+func (m *enigma) Decipher(text string) string {
 	for _, rotor := range m.rotors {
 		rotor.Reset()
 	}
 	return m.Encipher(text)
 }
 
-func (m *Machine) encipherCharacter(x rune) rune {
-	if strings.IndexRune(m.ALPHABET, x) == -1 {
-		return x
-	}
-	if !strings.ContainsRune(m.ALPHABET, x) {
-		return x
-	}
+func (m *enigma) encipherCharacter(x rune) rune {
 	contactIndex := strings.IndexRune(m.ALPHABET, x)
+	x = m.plugboard.Translate(rune(m.ALPHABET[contactIndex]))
 	for _, rotor := range m.rotors {
-		contactLetter := rune(rotor.alphabet[contactIndex])
+		contactLetter := rotor.alphabet[contactIndex]
 		x = rune(rotor.Encipher(byte(contactLetter)))
 		contactIndex = strings.IndexRune(rotor.alphabet, x)
 	}
-	contactLetter := rune(m.ALPHABET[contactIndex])
-	x = m.reflector.Reflect(contactLetter)
+	x = m.reflector.Reflect(rune(m.ALPHABET[contactIndex]))
 	contactIndex = strings.IndexRune(m.ALPHABET, x)
 	for i := len(m.rotors) - 1; i >= 0; i-- {
 		rotor := m.rotors[i]
-		contactLetter := rune(rotor.alphabet[contactIndex])
+		contactLetter := rotor.alphabet[contactIndex]
 		x = rune(rotor.Decipher(byte(contactLetter)))
 		contactIndex = strings.IndexRune(rotor.alphabet, x)
 	}
+	x = m.plugboard.Translate(rune(m.ALPHABET[contactIndex]))
 	m.rotors[0].Rotate(1)
-	for i := 1; i < len(m.rotors); i++ {
-		rotor := m.rotors[i]
-		turnFrequency := len(m.ALPHABET) * i
-		if m.rotors[i-1].rotations%turnFrequency == 0 {
-			rotor.Rotate(10)
-		}
-	}
 	return rune(m.ALPHABET[contactIndex])
+}
+
+type Plugboard struct {
+	mappings map[rune]rune
+}
+
+func NewPlugboard(mappingPairs []string) *Plugboard {
+	pb := &Plugboard{
+		mappings: make(map[rune]rune),
+	}
+	for _, pair := range mappingPairs {
+		pb.mappings[rune(pair[0])] = rune(pair[1])
+		pb.mappings[rune(pair[1])] = rune(pair[0])
+	}
+	return pb
+}
+
+func (pb *Plugboard) Translate(char rune) rune {
+	if mapped, ok := pb.mappings[char]; ok {
+		return mapped
+	}
+	return char
 }
 
 type Rotor struct {
 	initialOffset int
 	alphabet      string
-	rotations     int
 	forwardMap    map[byte]byte
 	reverseMap    map[byte]byte
 }
@@ -91,14 +102,12 @@ func NewRotor(mappings string, offset int) *Rotor {
 func (r *Rotor) Reset() {
 	r.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 	r.Rotate(r.initialOffset)
-	r.rotations = 1
 }
 
 func (r *Rotor) Rotate(offset int) {
 	for i := 0; i < offset; i++ {
 		r.alphabet = r.alphabet[1:] + string(r.alphabet[0])
 	}
-	r.rotations = offset
 }
 
 func (r *Rotor) Encipher(character byte) byte {
@@ -122,7 +131,6 @@ func NewReflector(mappings string) *Reflector {
 	for i, c := range ALPHABET {
 		refl.mappings[c] = rune(mappings[i])
 	}
-
 	return refl
 }
 
@@ -140,19 +148,19 @@ func main() {
 
 	// create a reflector
 	reflector := NewReflector("YRUHQSLDPXNGOKMIEBFZCWVJAT")
-
+	plugboard := NewPlugboard([]string{"REMJGQDXFSUIVYOBKTLPACNWZH"})
 	// create a machine with the three rotors and the reflector
-	machine := NewMachine([]*Rotor{rotor1, rotor2, rotor3, rotor4, rotor5}, reflector)
+	enigma := NewEnigma([]*Rotor{rotor1, rotor2, rotor3, rotor4, rotor5}, reflector, plugboard)
 
 	var text string
 	fmt.Print("Enter your message: ")
 	_, _ = fmt.Scanf("%s", &text)
 
-	encrypted := machine.Encipher(text)
+	encrypted := enigma.Encipher(text)
 	fmt.Printf("encrypted text is %s", encrypted)
 	fmt.Println()
 	// decipher the encrypted message
-	decrypted := machine.Decipher(encrypted)
+	decrypted := enigma.Decipher(encrypted)
 	fmt.Printf("decrypted text is %s", decrypted)
 
 }
